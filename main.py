30b75345653f1c15ccfcd4c58d03ec2979308200
Darrey1 <daretimileyin1@gmail.com>
2024-06-23 15:09:37 +0100

    new

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from UI.dashboard import dashboard_page
 from tkinter import *
 
 if __name__ == "__main__":
-    dashboard_page()
-    # root=Tk()
-    # app=Login(root)
-    # root.mainloop() 
+    root=Tk()
+    app=Login(root)
+    root.mainloop() 
     
